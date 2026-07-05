88488bd6dcca9dbab51dac0ca8e9ee396a94af92
niloy91926 <niloyahmednil987@gmail.com>
2026-07-05 13:28:05 +0600

    Add .env

--- bot.py
+++ bot.py
@@ -1,87 +1,27 @@
-sent_users = set()
+import logging
+from telethon import TelegramClient, events, Button
+from telethon.errors import UserNotParticipantError
+from telethon.tl.functions.channels import GetParticipantRequest
+from decouple import config
+from telethon.utils import get_display_name
 
-@BotzHub.on(events.ChatAction)
-async def _(event):
-    if on_join is False:
-        return
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger("ForceSubBot")
 
-    if not event.is_group:
-        return
+# ===== CONFIG =====
+BOT_TOKEN = config("BOT_TOKEN")
+API_ID = config("API_ID", cast=int)
+API_HASH = config("API_HASH")
 
-    if event.action_message:
-        return
+CHANNELS = config("CHANNELS").split()
 
-    if not (event.user_joined or event.user_added):
-        return
+WELCOME_MSG = config("WELCOME_MSG", default="Welcome {mention}")
+WELCOME_NOT_JOINED = config("WELCOME_NOT_JOINED", default="Join all channels first!")
 
-    user = await event.get_user()
+ON_JOIN = config("ON_JOIN", cast=bool, default=True)
+ON_NEW_MSG = config("ON_NEW_MSG", cast=bool, default=True)
 
-    # Bot-এর নিজের event ignore
-    me = await BotzHub.get_me()
-    if user.id == me.id:
-        return
+# ===== CLIENT =====
+bot = TelegramClient("ForceSubBot", API_ID, API_HASH).start(bot_token=BOT_TOKEN)
 
-    # একই user-এর জন্য একবারই
-    if user.id in sent_users:
-        return
-    sent_users.add(user.id)
-
-    chat = await event.get_chat()
-    title = chat.title or "this chat"
-    pp = await BotzHub.get_participants(chat)
-    count = len(pp)
-
-    mention = f"[{get_display_name(user)}](tg://user?id={user.id})"
-    name = user.first_name
-    last = user.last_name
-    fullname = f"{name} {last}" if last else name
-    username = f"@{user.username}" if user.username else mention
-
-    joined = await get_user_join(user.id)
-
-    if joined:
-        msg = welcome_msg.format(
-            mention=mention,
-            title=title,
-            fullname=fullname,
-            username=username,
-            name=name,
-            last=last,
-            channel=f"@{channel}",
-            count=count,
-        )
-
-        butt = [
-            [Button.url("Channel", f"https://t.me/{channel}")]
-        ]
-
-    else:
-        msg = welcome_not_joined.format(
-            mention=mention,
-            title=title,
-            fullname=fullname,
-            username=username,
-            name=name,
-            last=last,
-            channel=f"@{channel}",
-            count=count,
-        )
-
-        butt = [
-            [
-                Button.url("Channel 1", "https://t.me/night_gang_official_bot"),
-                Button.url("Channel 2", "https://t.me/night_support_group"),
-                Button.url("Channel 3", "https://t.me/night_premium_chanel"),
-            ],
-            [
-                Button.inline("UnMute Me", data=f"unmute_{user.id}")
-            ]
-        ]
-
-        await BotzHub.edit_permissions(
-            event.chat.id,
-            user.id,
-            send_messages=False
-        )
-
-    await event.reply(msg, buttons=butt)
+log.info("Bot started...")
